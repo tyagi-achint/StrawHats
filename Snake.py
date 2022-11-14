dc4ea0bdd7a8bcd138835ecbe47d07c950082605
ajayavd <dhondgeajay@gmail.com>
2022-11-14 16:49:24 +0530

    aja

---
+++ Snake.py
@@ -0,0 +1,14 @@
+import turtle
+import time
+import random
+#Setting Screen
+wn = turtle.Screen()
+wn.title("Snake Game By StrawHats")
+wn.bgcolor("blue")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+
+
+
+wn.mainloop()
